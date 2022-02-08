40a1093337d8fa9579066028527e6ac992f20f2c
Oleg-Klunovskiy <frenzi5814@gmail.com>
2022-02-08 21:29:25 +0200

    Merge remote-tracking branch 'github/master'

---
+++ js/class/4/main.js
@@ -0,0 +1,94 @@
+/*- створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)*/
+
+/*function abc(a, b, c) {
+    if (a < b && a < c) {
+        return a;
+    } else if (b < a && b < c) {
+        return b
+    } else {
+        return c
+    }
+
+}
+
+console.log(abc(4,66,20))*/
+
+/*- створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)*/
+/*function abc(a, b, c) {
+    if (a > b && a > c) {
+        return a;
+    } else if (b > a && b > c) {
+        return b
+    } else {
+        return c
+    }
+
+}
+console.log(abc(4,66,20))*/
+/*- створити функцію яка повертає найбільше число з масиву*/
+/*let arr = [54, 734, 124, 222, 7676, 234];
+
+function maxArr(array){
+    let max = 0;
+    for (const aElement of array) {
+        if (max < aElement) max = aElement
+
+    }
+    return  max;
+}
+
+let res = (maxArr(arr));
+console.log(res);*/
+
+/*- створити функцію яка повертає найменьше число з масиву*/
+/*let arr = [54, 734, 124, 222, 7676, 234];
+
+function maxArr(array){
+    let max = array[0];
+    for (const aElement of array) {
+        if (max > aElement) max = aElement
+
+    }
+    return  max;
+}
+
+let res = (maxArr(arr));
+console.log(res);*/
+/*- створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13*/
+/*var arr = [1, 2, 10];
+
+function arraySum(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    console.log(sum);
+}
+
+arraySum(arr)*/
+
+/*- створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.*/
+/*
+var arr = [1, 2, 3];
+
+function arraySum(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+        midle = sum/arr.length
+    }
+    console.log(midle);
+}
+
+arraySum(arr)
+*/
+
+
+/*- створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше
+(Math використовувати заборонено);
+- створити функцію яка заповнює масив рандомними числами
+(цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) та виводить його.
+- створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент,
+ який характеризує кінцеве значення діапазону.
+- Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
+*/
