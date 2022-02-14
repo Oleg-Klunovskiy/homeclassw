b04276e2fc9e75befd69fd7cdb9985370369516e
Oleg-Klunovskiy <frenzi5814@gmail.com>
2022-02-14 12:53:56 +0200

    Merge remote-tracking branch 'github/master'

--- js/class/6/main.js
+++ js/class/6/main.js
@@ -30,7 +30,7 @@ let name = (string, symbol) => {
 
 /*- створити функцію, яка генерує масив рандомних числових цілих значень в діапазоні від 0 до 100.*/
 
-let random = (length, num) => {
+/*let random = (length, num) => {
     let arr = [];
     for (let i = 0; i < length; i++) {
         arr.push(Math.floor(Math.random() * num));
@@ -38,30 +38,44 @@ let random = (length, num) => {
     return arr;
 };
 let result = random(3, 100);
-console.log(result);
+console.log(result);*/
 
 
 /*- створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень.
  Відсортувати його за допомоги sort*/
+/*
+console.log(result.sort((a, b) => a - b));*/
 
-console.log(result.sort((a, b) => a - b));
 
+/*- створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень.
+ відфільтрувати  його за допомоги filter, залишивши тільки парні числа*/
 
-/*- створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. відфільтрувати  його за допомоги filter, залишивши тільки парні числа*/
+/*console.log(result.filter(value => value % 2 === 0))*/
 
 
 /*- створити масив рандомних цілих числових значень (або згенерувати, за допомоги попередньої функції) .
 за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.*/
+/*
+console.log(result.map(value => value.toString()));*/
 
 /*- створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого,
     або навпаки в залежності від значення аргументу direction.
     let nums = [11,21,3];
 sortNums('ascending') // [3,11,21]
 sortNums('descending') // [21,11,3]*/
+/*
+
+let nums = [11, 21, 3];
+let sortNums = (arr, direction) => {
+    if (direction === 'asc') return arr.sort((a, b) => a - b);
+    if (direction === 'desc') return arr.sort((a, b) => b - a);
+}
+console.log(sortNums(nums, 'asc'));
+*/
 
 
-/*- є масив
-let coursesAndDurationArray = [
+/*- є масив*/
+/*let coursesAndDurationArray = [
     {title: 'JavaScript Complex', monthDuration: 5},
     {title: 'Java Complex', monthDuration: 6},
     {title: 'Python Complex', monthDuration: 6},
@@ -70,4 +84,16 @@ let coursesAndDurationArray = [
     {title: 'Frontend', monthDuration: 4}
 ];*/
 /*-- відсортувати його за спаданням за monthDuration*/
+
+/*coursesAndDurationArray.sort((a, b) => {
+    return a.monthDuration - b.monthDuration
+});
+console.log(coursesAndDurationArray);*/
+
+
 /*-- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців*/
+
+/*let filter = coursesAndDurationArray.filter((value) => {
+    return value.monthDuration > 5;
+});
+console.log(filter);*/
