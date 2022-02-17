b26a76215991364740c041dd4874c0a15bb0deec
Oleg-Klunovskiy <frenzi5814@gmail.com>
2022-02-17 11:15:22 +0200

    Merge remote-tracking branch 'github/master'

--- js/home/7/main.js
+++ js/home/7/main.js
@@ -63,3 +63,4 @@ clientArr = [
 /*- Взяти масив (Client [] з попереднього завдання).Відсортувати його по кількості товарів в полі order по зростанню. (sort)*/
 
 console.log(clientArr.sort((a, b) => a.order.length - b.order.length));
+
