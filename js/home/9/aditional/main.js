7c20581db0a9734675bc39404a2f7be96547146e
Oleg-Klunovskiy <frenzi5814@gmail.com>
2022-02-22 20:54:12 +0200

    Merge remote-tracking branch 'github/master'

--- js/home/9/aditional/main.js
+++ js/home/9/aditional/main.js
@@ -57,3 +57,32 @@ let coursesAndDurationArray = [
     let divElement = document.createElement('div');
     divElement.innerText = `${element.title} ${element.monthDuration}`;
     document.body.append(divElement);
+}*/
+
+/*- Є масив
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,  в якому буде <h1 class='heading'>
+ з title  елементу, та <p class='description'> з monthDuration елементу.
+    Завдання робити через цикли.
+*/
+for (const element of coursesAndDurationArray) {
+    let divElement = document.createElement('div');
+
+    let h4 = document.createElement('h4');
+    h4.innerText = element.title + ' триває';
+
+    let p = document.createElement('p');
+    p.innerText = element.monthDuration + ' місяці(в)';
+
+    divElement.appendChild(h4);
+    divElement.appendChild(p);
+    document.body.appendChild(divElement);
+}
+
